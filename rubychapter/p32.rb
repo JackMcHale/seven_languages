22ea2dbaf6ae95c7283c2abbbbd0b5589160d809
JackMcHale <jack.mchale@gmail.com>
2013-09-11 20:51:02 -0400

    version with loops

--- rubychapter/p32.rb
+++ rubychapter/p32.rb
@@ -1,5 +1,12 @@
 #Print the contents of an array
 #of 16 numbers four numbers at a time using each
 count = 0
-msg= '\n'
-[1,2,3,5,8,13,21,34,55,89,144,233,377,611,988,1599].each { |booj| puts booj }
+msg=" " 
+[1,2,3,5,8,13,21,34,55,89,144,233,377,611,988,1599].each do |booj| 
+	count +=1
+	print booj
+	print " "
+	if (count % 4) == 0 then
+		print "\n"
+	end
+end
